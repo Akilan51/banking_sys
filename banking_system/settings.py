4469a82c664d68513d377cc1c989a8692e75394a
akilanakilan832-spec <akilanakilan832@gmail.com>
2026-09-07 11:10:21 +0530

    add new update

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -86,6 +86,17 @@ WSGI_APPLICATION = 'banking_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'banking_system',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 # PostgreSQL database configuration
 #
 # Set these environment variables before running the project, or keep the
